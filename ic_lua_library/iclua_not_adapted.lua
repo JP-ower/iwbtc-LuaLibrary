e4920af998de7190ef1219baae0bf43e1fa3e510
smdkx <2024211534@stu.hit.edu.cn>
2025-05-05 14:13:51 +0800

    new file:   ic_lua_library/iclua_Game.lua 	modified:   ic_lua_library/iclua_not_adapted.lua

--- ic_lua_library/iclua_not_adapted.lua
+++ ic_lua_library/iclua_not_adapted.lua
@@ -2,7 +2,14 @@
 ComparisonOperator = {}
 ---@alias ComparisonOperator table
 
-
 ---2类比较运算符，== 或 !=
 ComparisonOperator2 = {}
 ---@alias ComparisonOperator2 table
+
+---数据类型
+VariableType = {}
+---@alias VariableType table
+
+---数据值
+VarData = {}
+---@alias VarData table
